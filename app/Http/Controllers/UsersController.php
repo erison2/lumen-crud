6a5cc44cdff36bb9a3b247354fe86538e4a2b4f4
Erison Quintela de Freitas <erisonquintela@hotmail.com>
2017-12-15 05:58:38 -0300

    Routes

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index() {
+        $users = User::all();
+        return $users;
+    }
+}
